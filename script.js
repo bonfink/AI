59e69ef67554e089b8b295305a9a15b9b97b412a
bonfink <126208614+bonfink@users.noreply.github.com>
2024-08-06 17:43:02 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,63 @@
 const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
-const caixaAlternativa = document.querySelector(".caixa-alternativa");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
-
 const perguntas = [
-  {
-    enunciado: "Pergunta 1",
-    alternativas: ["Alternativa 1", "Alternativa 2"],
-  },
-  {
-    enunciado: "Pergunta 2",
-    alternativas: ["Alternativa 1", "Alternativa 2"],
-  },
+    {
+        enunciado: "Você acha que AI pode ajudar na educação?",
+        alternativas: [
+            "sim",
+            "nao"
+        ]
+    },
+    {
+        enunciado: "aaa",
+        alternativas: [
+            "sim",
+            "nao"
+        ]
+    },
+    {
+        enunciado: "aaaaaaaa",
+        alternativas: [
+            "sim",
+            "nao"
+        ]
+    },
+    {
+        enunciado: "???",
+        alternativas: [
+            "sim",
+            "nao"
+        ]
+    },
+    {
+        enunciado: "perguntas",
+        alternativas: [
+           "sim",
+            "nao"
+        ]
+    },
 ];
+
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta() {
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    mostraAlternativas();
+}
+
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa;
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
+}
+
+mostraPergunta();
+
