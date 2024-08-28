5405814c247748215c06d7b0b3b370b9bf15013a
bonfink <126208614+bonfink@users.noreply.github.com>
2024-08-28 14:28:45 -0300

    Add files via upload

---
+++ js/perguntas.js
@@ -0,0 +1,108 @@
+export const perguntas = [
+    {
+        enunciado: "eu te odeio",
+        alternativas: [
+            {
+                texto: "ola",
+                afirmacao: [
+                    "eu te conheco?",
+                    "afirmacao 2",
+                    "afirmacao 3"
+                    
+                ],
+            },
+            {
+                texto: "(nao interajo)",
+                afirmacao: [
+                    "faz o certo",
+                    "afirmacao 2",
+                    "afirmacao 3"
+                ],
+            }
+        ]
+    },
+    {
+        enunciado: "oii",
+        alternativas: [
+            {
+                texto: "ola",
+                afirmacao: [
+                    "eu te conheco?",
+                    "afirmacao 2",
+                    "afirmacao 3"
+                ],
+            },
+            {
+                texto: "(nao interajo)",
+                afirmacao: [
+                    "faz o certo",
+                    "afirmacao 2",
+                    "afirmacao 3"
+                ],
+            }
+        ]
+    },
+    {
+        enunciado: "oii",
+        alternativas: [
+            {
+                texto: "ola",
+                afirmacao: [
+                    "eu te conheco?",
+                    "afirmacao 2",
+                    "afirmacao 3"
+                ],
+            },
+            {
+                texto: "(nao interajo)",
+                afirmacao: [
+                    "faz o certo",
+                    "afirmacao 2",
+                    "afirmacao 3"
+                ],
+            }
+        ]
+    },
+    {
+        enunciado: "oii",
+        alternativas: [
+            {
+                texto: "ola",
+                afirmacao: [
+                    "eu te conheco?",
+                    "afirmacao 2",
+                    "afirmacao 3"
+                ],
+            },
+            {
+                texto: "(nao interajo)",
+                afirmacao: [
+                    "faz o certo",
+                    "afirmacao 2",
+                    "afirmacao 3"
+                ],
+            }
+        ]
+    },
+    {
+        enunciado: "oii",
+        alternativas: [
+            {
+                texto: "ola",
+                afirmacao: [
+                    "eu te conheco?",
+                    "afirmacao 2",
+                    "afirmacao 3"
+                ]
+            },
+            {
+                texto: "(nao interajo)",
+                afirmacao: [
+                    "faz o certo",
+                    "afirmacao 2",
+                    "afirmacao 3"
+                ],
+            }
+        ]
+    },
+];
